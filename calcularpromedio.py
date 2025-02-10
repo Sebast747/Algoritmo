c2ea5a85de00cf494cf6f98af8867b35d0603937
Sebast747 <Osebastian388@gmail.com>
2025-02-10 11:24:43 -0600

    calcularPromedio
    se calculó el promedio y se pasó el codigo a phynton

---
+++ calcularpromedio.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+	cal1 = float()
+	cal2 = float()
+	cal3 = float()
+	cal4 = float()
+	cal5 = float()
+	promedio = float()
+	print("ingrese las 5 calificaciones")
+	cal1 = float(input())
+	cal2 = float(input())
+	cal3 = float(input())
+	cal4 = float(input())
+	cal5 = float(input())
+	promedio = (cal1+cal2+cal3+cal4+cal5)/5
+	print("El promedio de las calificaciones es: ",promedio)
